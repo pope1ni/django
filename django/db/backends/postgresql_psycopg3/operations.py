fa6bebd2577a75e932157932a0f837bf3219c1db
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-10-28 19:49:06 +0100

    Disable logging query for psycopg3

--- django/db/backends/postgresql_psycopg3/operations.py
+++ django/db/backends/postgresql_psycopg3/operations.py
@@ -213,8 +213,9 @@ class DatabaseOperations(BaseDatabaseOperations):
     def last_executed_query(self, cursor, sql, params):
         # https://www.psycopg.org/docs/cursor.html#cursor.query
         # The query attribute is a Psycopg extension to the DB API 2.0.
-        if cursor.query is not None:
-            return cursor.query.decode()
+        # TODO: psycopg3
+        # if cursor.query is not None:
+        #     return cursor.query.decode()
         return None
 
     def return_insert_columns(self, fields):
