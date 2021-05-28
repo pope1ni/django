386161373484178877238e8cc8bf5b957656fa58
Nick Pope <nick@nickpope.me.uk>
2021-05-29 00:04:25 +0100

    Refs #32943 -- Added support for covering exclusion constraints using SP-GiST indexes.

--- django/contrib/postgres/constraints.py
+++ django/contrib/postgres/constraints.py
@@ -45,10 +45,6 @@ class ExclusionConstraint(BaseConstraint):
             raise ValueError(
                 'ExclusionConstraint.include must be a list or tuple.'
             )
-        if include and index_type and index_type.lower() != 'gist':
-            raise ValueError(
-                'Covering exclusion constraints only support GiST indexes.'
-            )
         if not isinstance(opclasses, (list, tuple)):
             raise ValueError(
                 'ExclusionConstraint.opclasses must be a list or tuple.'
@@ -130,6 +126,13 @@ class ExclusionConstraint(BaseConstraint):
         ):
             raise NotSupportedError(
                 'Covering exclusion constraints using a GiST index requires PostgreSQL 12+.'
+            )
+        if (
+            self.include and self.index_type.lower() == 'spgist' and
+            not schema_editor.connection.features.supports_covering_spgist_indexes
+        ):
+            raise NotSupportedError(
+                'Covering exclusion constraints using an SP-GiST index requires PostgreSQL 14+.'
             )
 
     def deconstruct(self):
