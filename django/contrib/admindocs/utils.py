54cd9bca9b98244d8f938c3e91b8b92d0c064efb
Nick Pope <nick@nickpope.me.uk>
2021-07-08 00:19:35 +0100

    Fixed #32499 -- Escaped additional metacharacters in simplify_regex().

--- django/contrib/admindocs/utils.py
+++ django/contrib/admindocs/utils.py
@@ -147,7 +147,7 @@ def replace_metacharacters(pattern):
     Removes unescaped metacharacters from the pattern.
     """
     return re.sub(
-        r'((?:^|(?<!\\))(?:\\\\)*)(\\?)([?^$])',
+        r'((?:^|(?<!\\))(?:\\\\)*)(\\?)([?*+^$]|\\[bBAZ])',
         lambda m: m[1] + m[3] if m[2] else m[1],
         pattern,
     )
